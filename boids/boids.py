9a2c236e481ee8bc032a86ecf678031971205716
zcx <1098114942@qq.com>
2022-12-08 10:49:39 +0000

    reduce for loops

--- boids/boids.py
+++ boids/boids.py
@@ -22,8 +22,6 @@ def update_boids(boids):
     for i in range(len(xs)):
         for j in range(len(xs)):
             xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/len(xs)
-    for i in range(len(xs)):
-        for j in range(len(xs)):
             yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/len(xs)
     # Fly away from nearby boids
     for i in range(len(xs)):
@@ -32,8 +30,6 @@ def update_boids(boids):
                 xvs[i]=xvs[i]+(xs[i]-xs[j])
                 yvs[i]=yvs[i]+(ys[i]-ys[j])
     # Try to match speed with nearby boids
-    for i in range(len(xs)):
-        for j in range(len(xs)):
             if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
                 xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
                 yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
@@ -41,3 +37,5 @@ def update_boids(boids):
     for i in range(len(xs)):
         xs[i]=xs[i]+xvs[i]
         ys[i]=ys[i]+yvs[i]
+
+
